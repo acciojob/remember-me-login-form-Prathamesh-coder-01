f3efcc3d2098527423ddb1bd75cd028ddd786dfb
Prathamesh Dabhade <prathameshdabhade71@gmail.com>
2025-12-01 11:52:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// your JS code here. If required.
 const usernameInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
 const checkbox = document.getElementById("checkbox");
@@ -7,30 +8,38 @@ if (localStorage.getItem("username") && localStorage.getItem("password")) {
     existingBtn.style.display = "block";
 }
 
+
 existingBtn.addEventListener("click", () => {
     const savedUser = localStorage.getItem("username"); // read fresh value
     alert("Logged in as " + savedUser);
 });
 
+
 document.getElementById("loginForm").addEventListener("submit", function (e) {
-    e.preventDefault();
+    e.preventDefault(); // prevent page reload
 
     const username = usernameInput.value.trim();
     const password = passwordInput.value.trim();
 
+
     alert("Logged in as " + username);
 
+    
     if (checkbox.checked) {
         localStorage.setItem("username", username);
         localStorage.setItem("password", password);
     } else {
+       
         localStorage.removeItem("username");
         localStorage.removeItem("password");
     }
 
+    
     if (localStorage.getItem("username")) {
         existingBtn.style.display = "block";
     } else {
         existingBtn.style.display = "none";
     }
+});
+
 });
